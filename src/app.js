95d0985703c7c7d11057a9475c0e847b117222c4
akristhiam93 <a.kristhiam93@gmail.com>
2026-02-07 17:23:16 +0000

    exce

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+    function generateRandomCard () {
+      console.log("cartaAleatroria")
+    }
+    generateRandomCard ()
+
+ 
+    const value = ["As","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    
+
+    const suit = [
+        { symbol: "♥", color: "red" },
+        { symbol: "♦", color: "red" },
+        { symbol: "♠", color: "black" },
+        { symbol: "♣", color: "black" }
+    ]
+    
+   
+
+      let randomValue = value[Math.floor(Math.random() * value.length)]
+  
+      let randomSuit = suit[Math.floor(Math.random() * suit.length)]
+
+    document.getElementById("suit").innerHTML = randomSuit.symbol;
+    document.getElementById("suitInverted").innerHTML = randomSuit.symbol;
+    document.getElementById("value").innerHTML = randomValue;
+
+    document.getElementById("suit").style.color = randomSuit.color;
+    document.getElementById("suitInverted").style.color = randomSuit.color;
+    
+
+  
+  
+    
+  // dar estilo a la carta
+  //
+  // div para cada carta
+  //
+  //
+  //
+
+
+
+  
 };
+
