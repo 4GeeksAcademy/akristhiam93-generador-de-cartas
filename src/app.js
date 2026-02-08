15ee7266ea21defc839fd5db77e93bf8f0d7704d
akristhiam93 <a.kristhiam93@gmail.com>
2026-02-08 00:32:33 +0000

    add button and style

--- src/app.js
+++ src/app.js
@@ -1,46 +1,33 @@
 window.onload = function() {
   
-    function generateRandomCard () {
-      console.log("cartaAleatroria")
-    }
-    generateRandomCard ()
-
- 
-    const value = ["As","2","3","4","5","6","7","8","9","10","J","Q","K"]
+  function generateRandomCard () {
     
+  }
+  generateRandomCard ()
 
+    
+    const value = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    
     const suit = [
-        { symbol: "♥", color: "red" },
-        { symbol: "♦", color: "red" },
-        { symbol: "♠", color: "black" },
-        { symbol: "♣", color: "black" }
+      { symbol: "♥", color: "red" },
+      { symbol: "♦", color: "red" },
+      { symbol: "♠", color: "black" },
+      { symbol: "♣", color: "black" }
     ]
     
-   
 
-      let randomValue = value[Math.floor(Math.random() * value.length)]
-  
-      let randomSuit = suit[Math.floor(Math.random() * suit.length)]
-
+    let randomValue = value[Math.floor(Math.random() * value.length)]
+    
+    let randomSuit = suit[Math.floor(Math.random() * suit.length)]
+    
     document.getElementById("suit").innerHTML = randomSuit.symbol;
     document.getElementById("suitInverted").innerHTML = randomSuit.symbol;
     document.getElementById("value").innerHTML = randomValue;
-
+    
     document.getElementById("suit").style.color = randomSuit.color;
     document.getElementById("suitInverted").style.color = randomSuit.color;
     
-
-  
-  
     
-  // dar estilo a la carta
-  //
-  // div para cada carta
-  //
-  //
-  //
-
-
 
   
 };
