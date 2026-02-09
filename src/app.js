4ce5ca1429985e3d5e2706f58fcbd52746192657
akristhiam93 <a.kristhiam93@gmail.com>
2026-02-09 17:56:42 +0000

    add button and callback

--- src/app.js
+++ src/app.js
@@ -1,34 +1,33 @@
-window.onload = function() {
-  
-  function generateRandomCard () {
-    
-  }
+function generateRandomCard () {
+
+  const value = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+  const suit = [
+    { symbol: "♥", color: "red" },
+    { symbol: "♦", color: "red" },
+    { symbol: "♠", color: "black" },
+    { symbol: "♣", color: "black" }
+  ];
+
+  let randomValue = value[Math.floor(Math.random() * value.length)];
+  let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+
+  setInterval(myCallback, 5000);
+
+  function myCallback(a, b) {
   generateRandomCard ()
+}
 
-    
-    const value = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-    
-    const suit = [
-      { symbol: "♥", color: "red" },
-      { symbol: "♦", color: "red" },
-      { symbol: "♠", color: "black" },
-      { symbol: "♣", color: "black" }
-    ]
-    
+  document.getElementById("suit").innerHTML = randomSuit.symbol;
+  document.getElementById("suitInverted").innerHTML = randomSuit.symbol;
+  document.getElementById("value").innerHTML = randomValue;
 
-    let randomValue = value[Math.floor(Math.random() * value.length)]
-    
-    let randomSuit = suit[Math.floor(Math.random() * suit.length)]
-    
-    document.getElementById("suit").innerHTML = randomSuit.symbol;
-    document.getElementById("suitInverted").innerHTML = randomSuit.symbol;
-    document.getElementById("value").innerHTML = randomValue;
-    
-    document.getElementById("suit").style.color = randomSuit.color;
-    document.getElementById("suitInverted").style.color = randomSuit.color;
-    
-    
+  document.getElementById("suit").style.color = randomSuit.color;
+  document.getElementById("suitInverted").style.color = randomSuit.color;
+}
 
-  
+window.onload = function () {
+  generateRandomCard();
 };
+
 
